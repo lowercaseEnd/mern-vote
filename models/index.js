3c740cdb8f792528862e3b64e2bc0a6dc67824fd
John Doe <randomEme@protonmail.com>
2020-06-01 20:50:11 +0400

    изменил опции подключения к бд, добавил express-session, поключил паспорт

--- models/index.js
+++ models/index.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
-const options = {
-  url: `mongodb://localhost:27017/polls`,
-  db: polls
-};
-mongoose.connect(options.url, options);
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 db.on("error", err => {
   console.error(`Mongoose connection error: ${err}`);
 });
 db.once("open", () => {
-  console.info(`Mongoose default connection opened: ${options.db}`);
+  console.info(`Mongoose default connection opened: ${process.env.DB}`);
 });
 
 module.exports.User = require("./user.model");
 module.exports.Poll = require("./poll.model");
+module.exports.DB = db;
--- server.js
+++ server.js
@@ -3,11 +3,32 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const DB = require("./models/index");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+//инициализация паспорта
+const sessionOptions = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  },
+  store: new MongoStore({
+    mongooseConnection: DB.DB
+  })
+};
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", (req, res) => {
   res.send("Hello");
